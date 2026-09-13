fcd304994277f84b75e2da601475a4f35f208886
starvulkan <e3dw97d@aol.com>
2026-09-12 21:38:55 -0600

    Update main.js to make the placeholder a bit more fun
    made the placeholder demo a bit more fun it was boringgggg

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import './style.css'
 
 const app = document.querySelector('#app')
 
-app.innerHTML = `<h1>onFocus</h1>`
+app.innerHTML = `<h1>onFocus</h1>
+<p>hehehe welcome to the placeholder areaaa. work is in construction here!</p>
+<p>WE WILL BE UP AND RUNNING SOON!</p>
+`
 
 
